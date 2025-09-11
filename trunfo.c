b018a8e4c2270fc57a55c96b4f0e8413f4c7f90e
moniqueinez7 <229311220+moniqueinez7@users.noreply.github.com>
2025-09-11 02:03:33 +0000

    nivel mestre

--- trunfo.c
+++ trunfo.c
@@ -6,17 +6,22 @@ int main (){
 char estado;
 char codigo[4];
 char cidade[30];
-int populacao, turistico;
+unsigned long int populacao;
+long int turistico;
 float area, pib;
 float densidade, pibpercapita;
+float superpoder, inverso_densidade;
 
 //carta 2 
 char estado2;
 char codigo2[4];
 char cidade2[30];
-int populacao2, turistico2;
+unsigned long int populacao2;
+long int turistico2;
 float area2, pib2;
 float densidade2, pibpercapita2;
+float superpoder2, inverso_densidade2;
+
 
 printf("CARTA 1\n");
 
@@ -32,7 +37,7 @@ fgets(cidade, 30, stdin);
 cidade[strcspn(cidade, "\n")]= '\0';
 
 printf("Populaçao:\n");
-scanf ("%d", &populacao);
+scanf ("%lu", &populacao);
 
 printf("Area (KM):\n");
 scanf("%f", &area);
@@ -41,15 +46,19 @@ printf("PIB:\n");
 scanf("%f", &pib);
 
 printf("Numeros de pontos turisticos:\n");
-scanf("%d", &turistico);
+scanf("%ld", &turistico);
 
 densidade = (populacao/area);
 printf("Densidade populacional:%.2f\n ", densidade);
 getchar();
 
+inverso_densidade= (area/populacao);
+
 pibpercapita= (pib/(double)populacao);
-printf("PIB per capita: %f", pibpercapita);
+printf("PIB per capita: %.10f", pibpercapita);
 getchar ();
+
+superpoder= (populacao+area+pib+turistico+pibpercapita+inverso_densidade);
 
 printf("CARTA 2\n");
 
@@ -65,7 +74,7 @@ fgets(cidade2, 30, stdin);
 cidade2[strcspn(cidade, "\n")]= '\0';
 
 printf("Populaçao:\n");
-scanf ("%d", &populacao2);
+scanf ("%lu", &populacao2);
 
 printf("Area (KM):\n");
 scanf("%f", &area2);
@@ -74,16 +83,19 @@ printf("PIB:\n");
 scanf("%f", &pib2);
 
 printf("Numeros de pontos turisticos:\n");
-scanf("%d", &turistico2);
+scanf("%ld", &turistico2);
 
 densidade2 = (float) (populacao2/area2);
 printf("Densidade populacional:%f\n ", densidade2);
 getchar ();
 
+inverso_densidade2= (area2/populacao2);
+
 pibpercapita2= (pib2/(float)populacao2);
-printf ("PIB per capita: %f\n", pibpercapita2);
+printf ("PIB per capita: %.10f\n", pibpercapita2);
 getchar();
 
+superpoder2= (populacao2+area2+pib2+turistico2+pibpercapita2+inverso_densidade2);
 
 //saida formatada
 
@@ -92,12 +104,14 @@ printf("CARTA 1 \n");
 printf("estado: %c\n", estado);
 printf("Codigo: %s\n", codigo);
 printf("Nome da cidade: %s\n", cidade);
-printf("Populaçao: %d\n", populacao);
+printf("Populaçao: %lu\n", populacao);
 printf("Area: %.2f km\n", area);
 printf("PIB: %.2f bilhoes de reais\n", pib);
 printf("Numeros de pontos turisticos:\n", turistico);
 printf("Densidade populacional: %.2f hab/km\n ", densidade);
+printf("Inverso da Densidade: %f\n", inverso_densidade);
 printf("PIB per capita: %.2f reais\n", pibpercapita);
+printf("Super poder: %f.2\n", superpoder);
 
 
 //segunda carta 
@@ -105,12 +119,15 @@ printf("CARTA 2\n");
 printf("Estado: %c\n", estado);
 printf("Codigo: %s\n", codigo2); 
 printf("Nome da cidade: %s\n", cidade2);
-printf("Populaçao: %d\n", populacao2);
+printf("Populaçao: %lu\n", populacao2);
 printf("Area: %.2f km\n", area2);
 printf("PIB: %.2f bilhoes de reais\n", pib2);
 printf("Numeros de pontos de turistico: %d\n", turistico2);
 printf("Densidade populacional: %.2f hab/km\n", densidade2);
+printf ("Inverso da Densidade: %f\n", inverso_densidade2);
 printf("PIB per capita: %.2f reais\n", pibpercapita2);
+printf("Super poder: %f.2\n", superpoder2);
+
 
 return 0;
 
