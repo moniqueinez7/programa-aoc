fcdc9f1796dc0c06ceae23d6873f835ca8efb488
moniqueinez7 <229311220+moniqueinez7@users.noreply.github.com>
2025-09-23 18:48:10 +0000

    xadrez.c

--- tema4.c
+++ tema4.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+int main(){
+    int bispo=0, rainha=0;
+
+    //loop for 
+    printf ("TORRE:\n");
+    for(int torre=0; torre<5; torre++){
+        printf("Direita\n");//imprime a direçao do movimento
+    }
+    //loop while 
+    printf("\nBISPO:\n");
+    while (bispo<5){ 
+        printf("Cima, Direita\n");//imprime a direçao do movimento
+    bispo++;  
+    }
+    //loop do-while
+    printf("\nRAINHA\n");
+    do {
+        printf("Esquerda\n");
+        rainha++;
+    } while (rainha<8);
+
+
+
+
+
+
+
+
+    return 0;
+}
